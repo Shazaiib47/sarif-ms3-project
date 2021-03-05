ffe5218110e4bcbc434dc97263a2a88d289ec9c0
Shazaiib47 <shazaib47@hotmail.com>
2021-03-05 17:21:02 +0000

    Updated code that updates task into the DB, as well as removing and tidying up some code that had errors and linting errors within them

--- app.py
+++ app.py
@@ -57,9 +57,10 @@ def login():
         if existing_user:
             # ensures the entered password matches the field
             if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
+                   existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
                     return redirect(url_for(
                         "profile", username=session["user"]))
             else:
